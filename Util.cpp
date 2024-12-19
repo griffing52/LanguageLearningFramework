38227f8395e17e982e2c03c47141b90e74b37945
griffing52 <emailgriffinnow@gmail.com>
2024-12-18 17:58:52 -0800

    Attempt to add set to priority_queue

--- Util.cpp
+++ Util.cpp
@@ -6,4 +6,24 @@ namespace util {
 		os << word.value << '\t' << word.translation << '\t' << "C: " << word.complexity << ", A: " << word.age << ", F: " << word.frequency;
 		return os;
 	}
+
+	bool Compare::operator()(Word* below, Word* above)
+	{
+		if (below->complexity > above->complexity) {
+			return true;
+		}
+		else if (below->complexity == above->complexity) {
+			if (below->frequency > above->frequency) {
+				return true;
+			}
+			else if (below->frequency == above->frequency) {
+				if (below->age < above->age) {
+					return true;
+				}
+			}
+
+		}
+
+		return false;
+	}
 }
--- Util.h
+++ Util.h
@@ -33,33 +33,35 @@ namespace util {
 		}
 	};
 
-	struct Phrase : public Word {
-		set<Word*> words;
-		priority_queue<Phrase*> dependencies;
-		//priority_queue<Word*, vector<Word*>, Compare> dependencies;
-		//Word* dependencies = nullptr;
-	};
 
 	class Compare {
 	public:
-		bool operator()(Word* below, Word* above)
-		{
-			if (below->complexity > above->complexity) {
-				return true;
-			} else if (below->complexity == above->complexity) {
-				if (below->frequency > above->frequency) {
-					return true;
-				} else if (below->frequency == above->frequency) {
-					if (below->age < above->age) {
-						return true;
-					}
-				}
-				
-			}
+		bool operator()(Word* below, Word* above);
+	};
 
-			return false;
+	template <typename T>
+	class queue_set : public set<T> {
+	public:
+		const T& front() const {
+			return *this->begin();
+		}
+
+		void pop_back() {
+			this->erase(--this->end());
+		}
+
+		void push_back(const T& value) {
+			this->insert(value);
 		}
 	};
+
+	struct Phrase : public Word {
+		set<Word*> words;
+		//priority_queue<Phrase*> dependencies;
+		priority_queue<Word*, queue_set<Word*>, Compare> dependencies;
+		//Word* dependencies = nullptr;
+	};
+
 
 	ostream& operator<<(ostream& os, const Word& word);
 }
