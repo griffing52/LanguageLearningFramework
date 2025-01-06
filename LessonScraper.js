f8708e3a50fcf2edb244d8fdba9a5e9d227db9de
griffing52 <emailgriffinnow@gmail.com>
2025-01-06 15:13:07 -0800

    HTML to parsed json

--- LessonScraper.js
+++ LessonScraper.js
@@ -8,4 +8,20 @@
 // TODO seperate sentences on same line using puncuation like
 // What color do you want? Blue maybe?
 // into "What color do you want?" "Blue maybe?"
-[...document.querySelectorAll('b.sc-hHOBVR.jnovYm')].filter(elm => elm.innerHTML.trim() != '').map(elm => [elm.innerHTML.trim().replace(/[!"#$%&()*+,-./:;<=>?@[\]^_`{|}~]/g, ""), (elm.nextSibling.data || '').trim()])
+[...document.querySelectorAll('b.sc-hHOBVR.jnovYm')]
+    .filter(elm => elm.innerHTML.trim() != '')
+    //.map(elm => [elm.innerHTML.trim().replace(/[!.;]/g, "\n").split('\n')])
+    .map(elm => {
+        return {
+            'phrase': elm.innerHTML.trim().replace(/[?.;!"#$%&()*+,-./:;<=>?@[\]^_`{|}~]/g, ""),
+            'translation': (elm.nextSibling.data || '').trim(),
+            'phrase-original': elm.innerHTML.trim()
+        }
+    })
+    .filter(elm => elm.phrase != '' && elm.translation != '')
+    //.reduce((acc, elm) => {
+    //    elm.innerHTML.trim().replace(/[!.;]/g, "\n").split('\n')
+    //        .forEach(a => acc.push(a))
+    //    return acc
+    //}, [])
+    //.filter(elm => elm.trim() != '')
