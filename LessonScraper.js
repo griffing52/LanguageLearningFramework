f71ee275f56c4ecf64bba635dbb10b1a769eb643
griffing52 <emailgriffinnow@gmail.com>
2025-02-24 19:29:28 -0800

    include data file

--- LessonScraper.js
+++ LessonScraper.js
@@ -1,49 +1,27 @@
-let list = ['TaGKv5vfasIrcZahq6XM','VU0PQX4HSkG5L1LT2nIi','xR3otp4dCYUrOpin6sHh','6IZOAxuylsPxGU0bzdkt','RXSwNH8tAqYKWAb8pnzs','cARB92cO2qa0rUe6PrFQ','7whOzYP3TtxHGEsRLuBK','eXsX1LjnYeBzTLPaMEnQ','XyubmaAE3aYC1e8H4qxQ','B515ox30rBMZZDc5v4Y1','su9UBSUzrk5uiOiU502f','lfktOe0PdFKIOUkEeahj','hSISC0vuXqkQENfyIt20','X4zPazzolqQ65j72pl7X','CKnqrzWneK3jDQHzD9EI','gP1EPncU2hXFXJDfe8Jm','EmiF03VL19c9pdmzxIq2','lxfePfrPt1rabWExylkt','7WQuMci5P3sEl0QAlf5B','eF4dTTAdYxOglgToYpJ7','ghJGTXv1s8GcMLUucIxi','vzByGnsVihbDL3FpoWJ0','E1oNNEr2eELnv4Hvao8e','Nmay8XzLXbQWUWpBKMHQ','wqOV0aisjmFV2GalAEfv','cmDojH50kx3OBwbGgvdM','1mYnM5OTnuRa3M1IPBpk','pJdVnTuzIM6ps9JHrVbg','wW23LKVLV7fOD2PvpZCv','vZCoNXPGw67ajNwoRIRl','UIKc1EuELrn75fEQDOpf','TzJKFsvCwDvtKDACkfkM','qqr1RTHFlOTiV04hrQEr','wrkBh7B0OT9eJjZ7wPgZ','uFowikOylh0P2eZnAv31','yycFUeRhuAMBbcu2QlUo','8RWl7RmZ4BAF5WwnXY9o','8QfOOZz7Ebp9MNO4SMTz','QfXczr4v8KRxOP8sAVva','wl4Bx6Kqrfjs5ULVBh7M','qmQkZKO9VaJTtFadVy97','Lii0mMaSScNbVFBt3S9l','G0xtVyfmyHdIRYDgKEJD','sbF0tjrf13U7E8Hjv20x','55xBN7dKXxWHSSgmnM8G','1U0Dg9iDnPBmymBDcOih','t9kHuYrHNf0t93aHGhjm','zAevdGRHiogbKYac1VCJ','Am27EUyK2sA2kcjtNEDq','EB9UrPh02B0VIhO7lVoq','T90mL9dqz6kOYR8LhH9B','YH9vOmEcRLRzpFgq6UTs','jTb4N4amltbnYP3DYeg4','XfPUGPlZKkWrwQ8AbNYh','sSr4bFI5FPkiTtSANOlU','R7fKnet0RwBbHxLEoK0M','wivNvvy3FrFggOIizmUm','kHcSvOR1unQHxVLXJ4iy','uX6819FxKvrLxGzvGFo1','DE4QtrYVXo1QeSo4NNmY','rptUL7q7zcGyHt6TiTQZ','3ItfFAqqXiTTDTfVsHRx','4A2IVoC6sZnrNqxjAciT','GP2dingWXesmjzB1zFVy','BfiMPVeg1AELiFqqShqV','GX4qMVW11utm5eEX0UIg','Qq49BSXgZRIqHLhtrjNA','9iF0EKD0M87p7mjYdFoh','tCiBTYb8YgKIxZtg2C29','TlFuDiBjoggxNkIr1wGn','S2tFLKnHBhFWcLWGoIAK','DlV0ItXXjSbX51LzymkZ','rWM6wQJXQsRiUewdt5sn','OZ5e2VZU7n294XJNjXV0','3LQbwTgUoAVJ5fe7hx69','1a7aAGa3nstatLd2VYUm','0DmkOyNvvVA0veMXkIq9','Plh7T1WrwnXhSPsDyU7l','7uLKD8k3xD2VoNjjCPog','ErvdWQVhQEtqo0JRxlIU','61Gk4ie1ga6SZy2ftlxh','P34CHjXaWcuWXHklpsy7','K2lC17YWLyWmVUMPUg30','9EDrcKvV4vRH1F6uyYbl','c9Blhz0ww3LFhkgHpYjM','8rjEaArYaLjxRqbPs0NY','5j8YWe633UDRkqZoaLxU','HkblHRnLgyT8RK5lIN6n','JcvpcslzMKIMqkGsCnpl','4TmI2SdoUoudt6LUfaG4','meT8ja8pPRxxEtozMPz4','SJGvKLhdFq7InFE7ywIq','Bqv4OGl7T1nEXURqafOm','FtRMxp5QmzdXqOJr8k6i','IMVEKE0rGoerIOrgBbxL','5nv23qTvSF7MKEt0Cria','AYcpnesRAGAuoVAIvfQ5','jixp85fOShoFanaGHlRu','2GrAYidlzyYZ883KBLxJ','ftpjmIt9FxGnr7xQoXKI','7EQSj1S7D9xSmvMAVA5K','FULMG4RgInfvyUDTje3o','UgW78lgoDNXCTn41UfMV','T8fPryvkyHXhpX8x1PjL','q1GLkemjH3YhqPFcniuz','wKuPaim5frDrGSFaFwUp','PDhdLlyW3wdPfkRpICCG','W8aNhnXNBpeUsTqyrQ5J','aMRlLxyMQY5VkJUYZXWh','fYwpwEetVf3fyYj6yyhr','zB7q8V20g5mJ1g6D0YLD','T74wBfblrGvJoV2eg7iP','FWaUwMj8FPOwS9CcGqit','Z9aSKIzqfG4iG3lTv9Oz','52X5gT2eqgtjnDJLKe1k','pQJ6HWxsHGKKczAFxyg6','gf1HwXjrOoCjvqsT75W6','Xoo2ylHjE8RVSm4kODUU','HwMsIcUkukuNgEl0wt2c','dxyJvcXiru2dPHLyKHef','uos1kFdLho2xDMf79wC8','cd8aq37vnB12DtKOeC0r','CFpaoqjoXkGllmR7i76x','vfvQrj1VUvbsSXG8BJWt','OToFL07cPvKGM85y8PDh','enci5iNWvSk7UFZqhL34','0LmrKYPDWEkRDS7s0df3','dKxHndqAuchmcttLiVdb','1bB2rUZGsMfOmFt426MG','gDB3Jyiw0gwJXqZ33yeQ','jPILBZm0twoWJkE4sHOK','T7oPGe7JELOtHtImj9BJ','JDZcJVC53oe5yEgl26PI','cBCOsNXpBym5n1SbS3fi','2XQnEsUzWHBxHqbscOQ6','5O9YKT3MWElrirgCawbi','DEbphOpz0aLZJlr8YQgA','ZZlI8coYtK4DlnkuCURR','yxGzsFnqnCqWOWQPTzOp','I7l4xUsbatAcdyqN0LM1','N5Lh89wZLukD1J4zNV5t','KjBR2NfzWTKrsak3G5CS','z9eejGDgcIcyXtOGyNYH','bnj3vE6EwFJnErX0VI9A','89OhpTrhkmG0IxHVFQSJ','unCKzIz3mHetk47b7Cjl','H0EMzuVWkonhhNphmMWu','EZaH41FWcBAxTQuveA7c','4D7oDKYihBZatSgHQRcK','F5dVxevn4vop8mXT54jN','rl3TpUGa4pZb2CLAACBb','wCz1wfzOwnauX9YQjq15','lWKRghiH6dFO6X0KXjsp','g14LiBie8LBD4Ws9cAwz','tSYj7JoHDmpvxBHk1hLg','WJlyUaVbCRQ4Cs2hiFFK','hagZ0Gf3m2sjBI4tN6A5','VPjf7LmgA2pcsV2xZaXL','O6Xlggh2NQhESlEGEjgb','A3j0OyU7HpVP5ujdd3i0','jGxX7ILVSUPYJZm7vQwW','z3U0ByI3R0jt1PgenCEd','yvqCvDmWvCx8rHo4zpmf','GNAdqscJoFDzXbIFvnK0','xugHT2RxhZofZmFU1heO','2MPskGuNLQbWlbaVCeeC','dmDaoPbz77kda8jWwGWt','Sll2AAhnTOT8zbfgcsmA','kORMR2hKXPMGLy94jq2k','wGYjWZbDIYLdJ7jV8Bcv','UlWIVZxLE4az2YM05Pmf','82FavMW6bqYrmWurvg7k','UrJcXHf1NNFQWqbGioFP','fzcUpMsXZwP3G15i8QdW','o3cJug8WMe3Km7A0qyp7','MErYI0mbA2olnyxCyAdL','4CqyQiNlsRUgbA0wKnRB','OSrQjTjR3mvWOLkY7AoH','RYDhVAVZJHjeW1EUj8IW','GZmxgUGQBMQdpIRt5eCR','eSSJ3Inexy6XgTmtpovA','VlRPPPKR4nxSY9T0brrR','Hwgg9htkEtD1Mcc2EOba','IuCIWKpCUuZ01thsXtct','GjRD6MopTwL5FZdlSRC3','mejWaXSd4ttw9Wh6qTaQ','lMtOe5yeyAMz9x6YX1lh','pg2OJDIxz8RoG2nyU956','KbvSin8qwocDNx8bx0GV','5sCqu7kY1evPUP8D0S9k','V6fVBgGYzwoy7s3slugb','jcKBuPQA18kDZPthN8wD','qefnE2SIG2DbJb5eJRf6','QL3IXgpnNqhBY6PGKcEL','jqiq0slXPSXF88X8E0zV','x1k8YaOTMmvXT3uOjWuA','VQx4mlUwVBDF5F0qfJdm','LKZseaJ51nfQ3UdkElUD','uLLiF6XykQP6WnBurOZ8','B2BiCy7BGq76TdfXiroI','Fo3HwJwGF46qgFTMw6MG','fQikMrAEgC5y6HVSYq5p','OKhFZ1tNQ2DlUQDdz77k','uPaCt2kTi9CYXCyJfwmw','Pj8LSWhjHUIcKivIEutj','XgjI4Qv5oLdlCQmONaMb','maBnixWDAackyVEYs5ie','UGLp8taCMiJnFA1Sk4of','HP68hI3x3OaUTfX53lCI','IJtowagnsUnrpiNO2TCN','3dP0N0a04Cq6f2r7WsxS','pTPuJFLzy5w2URhPszjO','Xqn9UtlwJGscYq00SdW3','qrFbeFCYvsTejclV9o3A','Lgie5rjvsC5Ssbn6xBX9','utMx92K6tpKpB7PMYx9U','sgFErrploVv5AH9JzQ4I','gp6WWhiT1RskhbZzupvE','TztANzqGT6cuFi0qXpj1','GX3IEWfjtjsJh1FBZmdx','Lkdu7Iy9curTYUStwflz','hIaBvBPghTtWHF7U0Duf','biTSYkGdWJW4QXdrwo0Z','QCx0UiqBfDpVEAaZfTpc','99vQkqukmeimt0kHi25v','1cgk35WCT9i4ithrNb8s','1KZZsLMfxJTDpxu0nFIb','qreqvJqeCzQ3putvYw9O','zgPBt7p0p4QuSuLD2rhL','iChr7WUzrRWsDWUTf29L','2KfthuIwHhoQUBRhUD9j','RkbCzGbhvzYss8Pbolwr','BH11hNLCfyWqVH05NXUh','papVDkoHZBBwPSNbI6ev','Ikg1N1X3sa6FgjGeBE34','mVVDbic8xJTztATEiYB7','MTlpnRVgsEdvkkyZ2WBj','xXGdbX6ZvWPW0XM4Wz8R','EBjxmsqtow8esmXaR3Vy','K3G15HiAc1yhRSbboHk1','wsSTwEWcDToFEYpBzBx5','iT0oFArOkeKPdoArj7Oh','LMzdNDP38DrcWHesasLZ','YekwDLm4PUkaFC4JxAMV']
+// ==UserScript==
+// @name         LessonScraper
+// @namespace    http://tampermonkey.net/
+// @version      2025-02-25
+// @description  try to take over the world!
+// @author       You
+// @match        https://app.swiss-german-online.com/videos
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=swiss-german-online.com
+// @grant        none
+// ==/UserScript==
 
-let paused = false;
+(function () {
+    'use strict';
 
-document.addEventListener('keypress', e => {
-    if (e.key == 'p') {
-        if (paused) {
-            paused = false;
-            downloadAllAndMoveOn(true)
-        } else {
-            paused = true;
-        }
-    }
-})
+    // Your code here...
 
-// start process after loading page (gives one second + random interval btwn 0-1 seconds to load)
-setTimeout(() => {
-    setTimeout(() => {
-        if (paused) return;
+    console.log('starting');
+    let list = ['TaGKv5vfasIrcZahq6XM', 'VU0PQX4HSkG5L1LT2nIi', 'xR3otp4dCYUrOpin6sHh', '6IZOAxuylsPxGU0bzdkt', 'RXSwNH8tAqYKWAb8pnzs', 'cARB92cO2qa0rUe6PrFQ', '7whOzYP3TtxHGEsRLuBK', 'eXsX1LjnYeBzTLPaMEnQ', 'XyubmaAE3aYC1e8H4qxQ', 'B515ox30rBMZZDc5v4Y1', 'su9UBSUzrk5uiOiU502f', 'lfktOe0PdFKIOUkEeahj', 'hSISC0vuXqkQENfyIt20', 'X4zPazzolqQ65j72pl7X', 'CKnqrzWneK3jDQHzD9EI', 'gP1EPncU2hXFXJDfe8Jm', 'EmiF03VL19c9pdmzxIq2', 'lxfePfrPt1rabWExylkt', '7WQuMci5P3sEl0QAlf5B', 'eF4dTTAdYxOglgToYpJ7', 'ghJGTXv1s8GcMLUucIxi', 'vzByGnsVihbDL3FpoWJ0', 'E1oNNEr2eELnv4Hvao8e', 'Nmay8XzLXbQWUWpBKMHQ', 'wqOV0aisjmFV2GalAEfv', 'cmDojH50kx3OBwbGgvdM', '1mYnM5OTnuRa3M1IPBpk', 'pJdVnTuzIM6ps9JHrVbg', 'wW23LKVLV7fOD2PvpZCv', 'vZCoNXPGw67ajNwoRIRl', 'UIKc1EuELrn75fEQDOpf', 'TzJKFsvCwDvtKDACkfkM', 'qqr1RTHFlOTiV04hrQEr', 'wrkBh7B0OT9eJjZ7wPgZ', 'uFowikOylh0P2eZnAv31', 'yycFUeRhuAMBbcu2QlUo', '8RWl7RmZ4BAF5WwnXY9o', '8QfOOZz7Ebp9MNO4SMTz', 'QfXczr4v8KRxOP8sAVva', 'wl4Bx6Kqrfjs5ULVBh7M', 'qmQkZKO9VaJTtFadVy97', 'Lii0mMaSScNbVFBt3S9l', 'G0xtVyfmyHdIRYDgKEJD', 'sbF0tjrf13U7E8Hjv20x', '55xBN7dKXxWHSSgmnM8G', '1U0Dg9iDnPBmymBDcOih', 't9kHuYrHNf0t93aHGhjm', 'zAevdGRHiogbKYac1VCJ', 'Am27EUyK2sA2kcjtNEDq', 'EB9UrPh02B0VIhO7lVoq', 'T90mL9dqz6kOYR8LhH9B', 'YH9vOmEcRLRzpFgq6UTs', 'jTb4N4amltbnYP3DYeg4', 'XfPUGPlZKkWrwQ8AbNYh', 'sSr4bFI5FPkiTtSANOlU', 'R7fKnet0RwBbHxLEoK0M', 'wivNvvy3FrFggOIizmUm', 'kHcSvOR1unQHxVLXJ4iy', 'uX6819FxKvrLxGzvGFo1', 'DE4QtrYVXo1QeSo4NNmY', 'rptUL7q7zcGyHt6TiTQZ', '3ItfFAqqXiTTDTfVsHRx', '4A2IVoC6sZnrNqxjAciT', 'GP2dingWXesmjzB1zFVy', 'BfiMPVeg1AELiFqqShqV', 'GX4qMVW11utm5eEX0UIg', 'Qq49BSXgZRIqHLhtrjNA', '9iF0EKD0M87p7mjYdFoh', 'tCiBTYb8YgKIxZtg2C29', 'TlFuDiBjoggxNkIr1wGn', 'S2tFLKnHBhFWcLWGoIAK', 'DlV0ItXXjSbX51LzymkZ', 'rWM6wQJXQsRiUewdt5sn', 'OZ5e2VZU7n294XJNjXV0', '3LQbwTgUoAVJ5fe7hx69', '1a7aAGa3nstatLd2VYUm', '0DmkOyNvvVA0veMXkIq9', 'Plh7T1WrwnXhSPsDyU7l', '7uLKD8k3xD2VoNjjCPog', 'ErvdWQVhQEtqo0JRxlIU', '61Gk4ie1ga6SZy2ftlxh', 'P34CHjXaWcuWXHklpsy7', 'K2lC17YWLyWmVUMPUg30', '9EDrcKvV4vRH1F6uyYbl', 'c9Blhz0ww3LFhkgHpYjM', '8rjEaArYaLjxRqbPs0NY', '5j8YWe633UDRkqZoaLxU', 'HkblHRnLgyT8RK5lIN6n', 'JcvpcslzMKIMqkGsCnpl', '4TmI2SdoUoudt6LUfaG4', 'meT8ja8pPRxxEtozMPz4', 'SJGvKLhdFq7InFE7ywIq', 'Bqv4OGl7T1nEXURqafOm', 'FtRMxp5QmzdXqOJr8k6i', 'IMVEKE0rGoerIOrgBbxL', '5nv23qTvSF7MKEt0Cria', 'AYcpnesRAGAuoVAIvfQ5', 'jixp85fOShoFanaGHlRu', '2GrAYidlzyYZ883KBLxJ', 'ftpjmIt9FxGnr7xQoXKI', '7EQSj1S7D9xSmvMAVA5K', 'FULMG4RgInfvyUDTje3o', 'UgW78lgoDNXCTn41UfMV', 'T8fPryvkyHXhpX8x1PjL', 'q1GLkemjH3YhqPFcniuz', 'wKuPaim5frDrGSFaFwUp', 'PDhdLlyW3wdPfkRpICCG', 'W8aNhnXNBpeUsTqyrQ5J', 'aMRlLxyMQY5VkJUYZXWh', 'fYwpwEetVf3fyYj6yyhr', 'zB7q8V20g5mJ1g6D0YLD', 'T74wBfblrGvJoV2eg7iP', 'FWaUwMj8FPOwS9CcGqit', 'Z9aSKIzqfG4iG3lTv9Oz', '52X5gT2eqgtjnDJLKe1k', 'pQJ6HWxsHGKKczAFxyg6', 'gf1HwXjrOoCjvqsT75W6', 'Xoo2ylHjE8RVSm4kODUU', 'HwMsIcUkukuNgEl0wt2c', 'dxyJvcXiru2dPHLyKHef', 'uos1kFdLho2xDMf79wC8', 'cd8aq37vnB12DtKOeC0r', 'CFpaoqjoXkGllmR7i76x', 'vfvQrj1VUvbsSXG8BJWt', 'OToFL07cPvKGM85y8PDh', 'enci5iNWvSk7UFZqhL34', '0LmrKYPDWEkRDS7s0df3', 'dKxHndqAuchmcttLiVdb', '1bB2rUZGsMfOmFt426MG', 'gDB3Jyiw0gwJXqZ33yeQ', 'jPILBZm0twoWJkE4sHOK', 'T7oPGe7JELOtHtImj9BJ', 'JDZcJVC53oe5yEgl26PI', 'cBCOsNXpBym5n1SbS3fi', '2XQnEsUzWHBxHqbscOQ6', '5O9YKT3MWElrirgCawbi', 'DEbphOpz0aLZJlr8YQgA', 'ZZlI8coYtK4DlnkuCURR', 'yxGzsFnqnCqWOWQPTzOp', 'I7l4xUsbatAcdyqN0LM1', 'N5Lh89wZLukD1J4zNV5t', 'KjBR2NfzWTKrsak3G5CS', 'z9eejGDgcIcyXtOGyNYH', 'bnj3vE6EwFJnErX0VI9A', '89OhpTrhkmG0IxHVFQSJ', 'unCKzIz3mHetk47b7Cjl', 'H0EMzuVWkonhhNphmMWu', 'EZaH41FWcBAxTQuveA7c', '4D7oDKYihBZatSgHQRcK', 'F5dVxevn4vop8mXT54jN', 'rl3TpUGa4pZb2CLAACBb', 'wCz1wfzOwnauX9YQjq15', 'lWKRghiH6dFO6X0KXjsp', 'g14LiBie8LBD4Ws9cAwz', 'tSYj7JoHDmpvxBHk1hLg', 'WJlyUaVbCRQ4Cs2hiFFK', 'hagZ0Gf3m2sjBI4tN6A5', 'VPjf7LmgA2pcsV2xZaXL', 'O6Xlggh2NQhESlEGEjgb', 'A3j0OyU7HpVP5ujdd3i0', 'jGxX7ILVSUPYJZm7vQwW', 'z3U0ByI3R0jt1PgenCEd', 'yvqCvDmWvCx8rHo4zpmf', 'GNAdqscJoFDzXbIFvnK0', 'xugHT2RxhZofZmFU1heO', '2MPskGuNLQbWlbaVCeeC', 'dmDaoPbz77kda8jWwGWt', 'Sll2AAhnTOT8zbfgcsmA', 'kORMR2hKXPMGLy94jq2k', 'wGYjWZbDIYLdJ7jV8Bcv', 'UlWIVZxLE4az2YM05Pmf', '82FavMW6bqYrmWurvg7k', 'UrJcXHf1NNFQWqbGioFP', 'fzcUpMsXZwP3G15i8QdW', 'o3cJug8WMe3Km7A0qyp7', 'MErYI0mbA2olnyxCyAdL', '4CqyQiNlsRUgbA0wKnRB', 'OSrQjTjR3mvWOLkY7AoH', 'RYDhVAVZJHjeW1EUj8IW', 'GZmxgUGQBMQdpIRt5eCR', 'eSSJ3Inexy6XgTmtpovA', 'VlRPPPKR4nxSY9T0brrR', 'Hwgg9htkEtD1Mcc2EOba', 'IuCIWKpCUuZ01thsXtct', 'GjRD6MopTwL5FZdlSRC3', 'mejWaXSd4ttw9Wh6qTaQ', 'lMtOe5yeyAMz9x6YX1lh', 'pg2OJDIxz8RoG2nyU956', 'KbvSin8qwocDNx8bx0GV', '5sCqu7kY1evPUP8D0S9k', 'V6fVBgGYzwoy7s3slugb', 'jcKBuPQA18kDZPthN8wD', 'qefnE2SIG2DbJb5eJRf6', 'QL3IXgpnNqhBY6PGKcEL', 'jqiq0slXPSXF88X8E0zV', 'x1k8YaOTMmvXT3uOjWuA', 'VQx4mlUwVBDF5F0qfJdm', 'LKZseaJ51nfQ3UdkElUD', 'uLLiF6XykQP6WnBurOZ8', 'B2BiCy7BGq76TdfXiroI', 'Fo3HwJwGF46qgFTMw6MG', 'fQikMrAEgC5y6HVSYq5p', 'OKhFZ1tNQ2DlUQDdz77k', 'uPaCt2kTi9CYXCyJfwmw', 'Pj8LSWhjHUIcKivIEutj', 'XgjI4Qv5oLdlCQmONaMb', 'maBnixWDAackyVEYs5ie', 'UGLp8taCMiJnFA1Sk4of', 'HP68hI3x3OaUTfX53lCI', 'IJtowagnsUnrpiNO2TCN', '3dP0N0a04Cq6f2r7WsxS', 'pTPuJFLzy5w2URhPszjO', 'Xqn9UtlwJGscYq00SdW3', 'qrFbeFCYvsTejclV9o3A', 'Lgie5rjvsC5Ssbn6xBX9', 'utMx92K6tpKpB7PMYx9U', 'sgFErrploVv5AH9JzQ4I', 'gp6WWhiT1RskhbZzupvE', 'TztANzqGT6cuFi0qXpj1', 'GX3IEWfjtjsJh1FBZmdx', 'Lkdu7Iy9curTYUStwflz', 'hIaBvBPghTtWHF7U0Duf', 'biTSYkGdWJW4QXdrwo0Z', 'QCx0UiqBfDpVEAaZfTpc', '99vQkqukmeimt0kHi25v', '1cgk35WCT9i4ithrNb8s', '1KZZsLMfxJTDpxu0nFIb', 'qreqvJqeCzQ3putvYw9O', 'zgPBt7p0p4QuSuLD2rhL', 'iChr7WUzrRWsDWUTf29L', '2KfthuIwHhoQUBRhUD9j', 'RkbCzGbhvzYss8Pbolwr', 'BH11hNLCfyWqVH05NXUh', 'papVDkoHZBBwPSNbI6ev', 'Ikg1N1X3sa6FgjGeBE34', 'mVVDbic8xJTztATEiYB7', 'MTlpnRVgsEdvkkyZ2WBj', 'xXGdbX6ZvWPW0XM4Wz8R', 'EBjxmsqtow8esmXaR3Vy', 'K3G15HiAc1yhRSbboHk1', 'wsSTwEWcDToFEYpBzBx5', 'iT0oFArOkeKPdoArj7Oh', 'LMzdNDP38DrcWHesasLZ', 'YekwDLm4PUkaFC4JxAMV'];
+    console.log('loaded');
 
-        downloadAllAndMoveOn()
-    }, Math.floor((Math.random() * 1000)))
+    let paused = false;
 
-}, 1000)
-
-
-// get phrases, saves them, and moves on to next page
-const downloadAllAndMoveOn = () => {
-    let currPage = window.location.href.split('/').pop();
-    let currIdx = list.indexOf(currPage)
-
-    if (currIdx == -1) return;
-
-    let phrases = getPhrases()
-    saveFile(JSON.stringify(phrases), '_'+currPage, 'json')
-    saveFile(phrases.map(a => `${a['phrase']}=${a['translation']}\n`).reduce((acc, elm) => acc + elm), currPage, 'txt')
-
-    //if (save_audio) saveAudio()
-
-
-    window.location.href = 'https://app.swiss-german-online.com/videos/' + list[currIdx + 1]
-}
-
-
-// saves audio on page
-//async function saveAllAudio(pageName) {
+    // saves audio on page
+    //async function saveAllAudio(pageName) {
     //const audioElements = document.querySelectorAll('source');
 
     //var i = 0;
@@ -62,44 +40,82 @@ const downloadAllAndMoveOn = () => {
     //    }
     //    i++
     //}
-//}
+    //}
 
-/**
- * 
+    /**
+ *
  * @param {string} content
  * @param {string} currPage
  * @param {string} extension file extension such as 'json' or 'txt'
  */
-const saveFile = (content, currPage, extension) => {
-    var a = document.createElement("a");
-    var file = new Blob([content], { type: 'text/plain' });
-    a.href = URL.createObjectURL(file);
-    a.download = currPage+'.'+extension;
-    a.click();
-}
+    const saveFile = (content, currPage, extension) => {
+        var a = document.createElement("a");
+        var file = new Blob([content], { type: 'text/plain' });
+        a.href = URL.createObjectURL(file);
+        a.download = currPage + '.' + extension;
+        a.click();
+    }
 
-// This gathers all the phrases and translations from the lesson
-// TODO seperate sentences on same line using puncuation like
-// What color do you want? Blue maybe?
-// into "What color do you want?" "Blue maybe?"
-const getPhrases = () =>
-[...document.querySelectorAll('b.sc-hHOBVR.jnovYm')]
-    .filter(elm => elm.innerHTML.trim() != '')
-    //.map(elm => [elm.innerHTML.trim().replace(/[!.;]/g, "\n").split('\n')])
-    .map(elm => {
-        let string = elm.innerHTML.trim().replace(/[?.;!"#$%&()*+,-./…:;<=>?@[\]^_`{|}~]/g, "")
+    // This gathers all the phrases and translations from the lesson
+    // TODO seperate sentences on same line using puncuation like
+    // What color do you want? Blue maybe?
+    // into "What color do you want?" "Blue maybe?"
+    const getPhrases = () =>
+        [...document.querySelectorAll('b.sc-hHOBVR.jnovYm')]
+            .filter(elm => elm.innerHTML.trim() != '')
+            //.map(elm => [elm.innerHTML.trim().replace(/[!.;]/g, "\n").split('\n')])
+            .map(elm => {
+                let string = elm.innerHTML.trim().replace(/[?.;!"#$%&()*+,-./…:;<=>?@[\]^_`{|}~]/g, "")
 
-        return {
-            'phrase': string.charAt(0).toUpperCase() + string.slice(1),
-            'translation': (elm.nextSibling.data || '').trim().replaceAll('’', '\''),
-            'phrase-original': elm.innerHTML.trim()
+                return {
+                    'phrase': string.charAt(0).toUpperCase() + string.slice(1),
+                    'translation': (elm.nextSibling.data || '').trim().replaceAll('’', '\''),
+                    'phrase-original': elm.innerHTML.trim()
+                }
+            })
+            .filter(elm => elm.phrase != '' && elm.translation != '');
+
+
+    // get phrases, saves them, and moves on to next page
+    const downloadAllAndMoveOn = () => {
+        let currPage = window.location.href.split('/').pop();
+        let currIdx = list.indexOf(currPage)
+
+        if (currIdx == -1) return;
+
+        let phrases = getPhrases()
+        saveFile(JSON.stringify(phrases), '_' + currPage, 'json')
+        saveFile(phrases.map(a => `${a.phrase}=${a.translation}\n`).reduce((acc, elm) => acc + elm), currPage, 'txt')
+
+        //if (save_audio) saveAudio()
+
+
+        window.location.href = 'https://app.swiss-german-online.com/videos/' + list[currIdx + 1]
+    }
+
+    document.addEventListener('keypress', e => {
+        if (e.key == 'p') {
+            if (paused) {
+                paused = false;
+                downloadAllAndMoveOn(true);
+            } else {
+                paused = true;
+            }
         }
     })
-    .filter(elm => elm.phrase != '' && elm.translation != '')
 
-    //.reduce((acc, elm) => {
-    //    elm.innerHTML.trim().replace(/[!.;]/g, "\n").split('\n')
-    //        .forEach(a => acc.push(a))
-    //    return acc
-    //}, [])
-    //.filter(elm => elm.trim() != '')
+    // start process after loading page (gives one second + random interval btwn 0-1 seconds to load)
+    console.log('set timeout 1')
+    setTimeout(() => {
+        setTimeout(() => {
+            console.log('timeout 1 run')
+            if (paused) return;
+
+            console.log('downloading...');
+
+            downloadAllAndMoveOn()
+        }, Math.floor((Math.random() * 1000)))
+
+    }, 1000)
+
+})();
